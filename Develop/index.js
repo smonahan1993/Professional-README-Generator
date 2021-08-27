5080c7d8afaaa67dfacbcfdcb6c6093a7564b70b
Sean Monahan <sean.r.monahan3@gmail.com>
2021-08-27 10:17:11 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,34 @@ var fs = require("fs");
 var inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input", 
+        message: "What is your project title?",
+        name: "title"
+    },{
+        type: "input",
+        message: "Please enter a description of your project.",
+        name: "description"
+    },{
+        type: "input",
+        message: "What are the installation instructions?",
+        name: "installation"
+    },{
+        type: "input",
+        message: "What is the project's usage information?",
+        name: "usage"
+    },{
+        type: "input",
+        message: "What are the contribution guidelines?",
+        name: "contribution"
+    },{
+        type: "input",
+        message: "What are the test instructions?",
+        name: "test"
+    }
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
